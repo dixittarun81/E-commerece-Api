72627e8801468839f65a8147b34b0e48daff4e25
tarun dixit <dixittarun81@gmail.com>
2020-08-29 19:22:03 +0530

    database setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const env = require('dotenv');
 const app = express();
-
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose'); 
 env.config();
+app.use(bodyParser.json());
+
+
+
+
+
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.dtgaf.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, 
+    {useNewUrlParser: true,
+    useUnifiedTopology: true}
+).then(() => {
+    console.log('Database connected');
+})
+
+app.get('/',(req,res,next) => {
+    res.status(200).json({
+        message: "Hello from server"
+    })
+})
+
+
+
+app.post('/data',(req,res,next) => {
+    res.status(200).json({
+        message: req.body,
+    })
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 })
+
+//
